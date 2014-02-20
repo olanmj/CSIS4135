85d439889ab98c44c554c6b5df058d8cd79f7078
olanmj <olanm1@msn.com>
2014-02-19 22:08:52 -0500

    Dang

--- MVCCalc/Models/MPGCalculator.cs
+++ MVCCalc/Models/MPGCalculator.cs
@@ -7,9 +7,6 @@ namespace MVCCalc.Models
 {
     public class MPGCalculator
     {
-        public double Gallons { get; set; }
-        public double Miles { get; set; }
-
         public double CalculateMPG(double miles, double gallons) {
             return miles / gallons;
         }
